8a8e3777fabc9643c44d2e62f4002026306ed183
Akshat Shukla <ashukla20062006@gmail.com>
2026-06-15 10:36:51 +0530

    outlet removal

--- env.js
+++
@@ -1,6 +0,0 @@
-// Generated env for ipTracker (do not commit real secrets)
-// Use IPGEO_API_KEY=... in .env, then copy value into this file.
-window.__ENV__ = {
-  IPGEO_API_KEY: "5ded3477bb9d42f381b552f447b82ead"
-};
-
